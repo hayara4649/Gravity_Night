e95bee02ca5840c5644a5eee6aee63a2ec5ea8a8
hayara0525 <hayara0525@gmail.com>
2020-09-21 16:39:30 +0900

    Playerの重力を変更できるようにしました

---
+++ Assets/CreateStage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStage : MonoBehaviour
+{
+    // public GameObject wall;
+	// public GameObject moveWall;
+	// public GameObject particalWall;
+	// float timer =0.0f;
+	// int interval=3;
+	// float posy;
+	// public float mainPos=4.0f;
+
+	// private void Start() {
+	// 	posy=wall.transform.position.y+mainPos;	
+	// 	Debug.Log(posy);
+	// }
+	
+	// void Update () {
+	// 	timer+=Time.deltaTime;
+
+	// 	if(timer >= interval){
+	// 		int i=Random.Range(0,3);
+	// 		// transform.position=new Vector3(0,Random.Range(posy-1.5f,posy+1.5f), transform.position.z);
+	// 		transform.position=new Vector3(0, Random.Range(posy-1.0f, posy+1.0f), transform.position.z);
+	// 		Debug.Log(posy);
+
+	// 		if(i==0){
+	// 			Instantiate(wall, transform.position, transform.rotation);
+	// 		} else if(i==1){
+	// 			Instantiate(moveWall, transform.position, transform.rotation);
+	// 		} else if(i==2){
+	// 			Instantiate(particalWall, transform.position, transform.rotation);
+	// 		}
+	// 		timer=0;
+
+	// 	}
+
+	// }
+}
---
+++ Assets/Scripts/ChangeGravity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGravity : MonoBehaviour
+{
+    [SerializeField] private Vector3 localGravity;
+    private Rigidbody rbody;
+    float direction;
+
+    void Start()
+    {
+        rbody = this.GetComponent<Rigidbody>();
+        rbody.useGravity = false;
+        
+        direction =localGravity.y;
+        Debug.Log(direction);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SetLocalGravity();
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            direction *= -1;
+            Debug.Log(direction);
+
+            localGravity = new Vector3(0,direction,0);
+        }
+    }
+
+    private void SetLocalGravity()
+    {
+        rbody.AddForce(localGravity, ForceMode.Acceleration);
+    }
+}
